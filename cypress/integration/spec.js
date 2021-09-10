2c1f4fc0bc46558e4a05009dfd058a59d7382121
Giuliano Giacaglia <raulzito234@gmail.com>
2021-09-10 11:26:23 -0700

    Updating the test

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -5,22 +5,26 @@ describe("page", () => {
 		console.log("before");
 	});
 
-	beforeEach(() => {
-		console.log("Before each");
-		cy.visit("localhost:3000/test3");
+	it("Visit react app", () => {
+		cy.visit("localhost:3000");
+		cy.get("button").click();
 	});
+	// beforeEach(() => {
+	// 	console.log("Before each");
+	// 	cy.visit("localhost:3000/test3");
+	// });
 
-	arrayValues.forEach((value) => {
-		it(`Localhost - ${value}`, () => {
-			cy.visit("localhost:3000");
-		});
+	// arrayValues.forEach((value) => {
+	// 	it(`Localhost - ${value}`, () => {
+	// 		cy.visit("localhost:3000");
+	// 	});
 
-		it(`Localhost test - ${value}`, () => {
-			cy.visit("localhost:3000/test");
-		});
+	// 	it(`Localhost test - ${value}`, () => {
+	// 		cy.visit("localhost:3000/test");
+	// 	});
 
-		it(`Localhost test2 - ${value}`, () => {
-			cy.visit("localhost:3000/test2");
-		});
-	});
+	// 	it(`Localhost test2 - ${value}`, () => {
+	// 		cy.visit("localhost:3000/test2");
+	// 	});
+	// });
 });
